357cac00c8880bcd970a9337a4946032c1d5e00c
Yash Tyagi <92726355+yashkt2525@users.noreply.github.com>
2023-10-29 11:43:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 console.log("Hello");
+let count = 0;
 const outputDiv = document.getElementById("output");
-outputDiv.innerText = "hello";
+outputDiv.innerText = count;
+
+function add() {
+  count = count + 1;
+  outputDiv.innerText = count;
+}
+function sub() {
+  count = count - 1;
+  outputDiv.innerText = count;
+}
